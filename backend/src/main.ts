e9ffedfff637497d774b631dbcf841d04c1dd86d
kamimi01 <awakaru0122@gmail.com>
2021-08-01 14:27:39 +0900

    feat: OpenAPI SpecをGithub Pagesで表示できるようにした

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
+import { dump } from 'js-yaml';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
-    .setTitle('Praha ChallengeをDDDで実装')
+    .setTitle('Praha ChallengeをDDDで実装する')
+    .setDescription('Praha ChallengeをDDDで実装する')
+    .setVersion('1.0')
+    .addTag('example')
     .build();
   const document = SwaggerModule.createDocument(app, options);
+
+  // YAML
+  fs.writeFileSync('/workspaces/Node_Projects/implementing-domain-driven-design/docs/openapi-spec.yml', dump(document, {}));
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
