de33049e605416caa2800394eec66ad4d1cd7456
kamimi01 <awakaru0122@gmail.com>
2021-08-29 17:29:50 +0900

    feat: emailのバリューオブジェクトの作成

--- backend/src/domain/participant/value-object/email.ts
+++ backend/src/domain/participant/value-object/email.ts
@@ -7,29 +7,25 @@ interface EmailProps {
 export class Email extends AbstractValueObject<EmailProps> {
   private readonly props: EmailProps;
 
-  private constructor(props: EmailProps) {
+  constructor(props: EmailProps) {
     super(props);
 
     if (!this.isValid(props)) {
       throw new Error('Emailアドレスの形式が誤っています');
     }
 
-    this.props = props
+    this.props = props;
   }
 
   private isValid(props: EmailProps): boolean {
     const reg =
       /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}.[A-Za-z0-9]{1,}$/;
 
-    if (
-      props.email === undefined ||
-      props.email === null ||
-      !reg.test(props.email)
-    ) {
+    if (!reg.test(props.email)) {
       return false;
-    } else {
-      return true;
     }
+
+    return true;
   }
 
   get email(): string {
